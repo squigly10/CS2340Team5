f6aa4182ecbcd8fc78c3454356dcf15de8f98f76
tom <tom.hightower356@gmail.com>
2018-01-29 18:28:57 -0500

    Completed the method in Person2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,5 +1,8 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.*;
+import java.lang.*;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
@@ -31,7 +34,16 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+      List<Character> inChars = new ArrayList<Character>();
+      for(char i:input.toCharArray()){
+          inChars.add(i);
+      }
+      StringBuilder returnString = new StringBuilder(input.length());
+      while(inChars.size()!=0){
+          int randomNumber = (int)(Math.random()*characters.size());
+          returnString.append(inChars.remove(randomNumber));
+      }
+	  return returnString.toString();
 	}
 	/**
 	 * Return a string rep of this object
