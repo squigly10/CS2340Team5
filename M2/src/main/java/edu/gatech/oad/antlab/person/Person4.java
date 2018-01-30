8fc4e0bf30041ed6c3e45d970aeda47d711802d3
hjsmulders <hjsmulders@gmail.com>
2018-01-30 14:34:36 -0500

    edit person4

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,13 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String retval;
+      for (int i = 0; i < input.length(); i++) {
+        char temp = input.charAt(i);
+        temp = temp + 1;
+        retval = retval + temp;
+      }
+      return retval;
     }
     
     /**
