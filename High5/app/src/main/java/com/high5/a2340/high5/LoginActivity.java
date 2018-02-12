d83678621a7c551896439b5fc95c5d852fe2ba47
yasaad <youssef.a.asaad@gmail.com>
2018-02-12 15:46:02 -0500

    Login in progress

--- High5/app/src/main/java/com/high5/a2340/high5/LoginActivity.java
+++ High5/app/src/main/java/com/high5/a2340/high5/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.high5.a2340.high5;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import org.w3c.dom.Text;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "EmailPassword";
 
     private FirebaseAuth fireBaseAuth;
 
@@ -28,6 +42,9 @@ public class LoginActivity extends AppCompatActivity {
         passwordText = (EditText) findViewById((R.id.passwordTextBox));
         signUpButton = (TextView) findViewById((R.id.signInButton));
 
+        loginButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+
     }
 
 
@@ -36,7 +53,44 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = fireBaseAuth.getCurrentUser();
-        updateUI(currentUser);
+        // DO THIS
+        // got to main screen
     }
 
+    public void signIn() {
+        String email = emailText.getText().toString().trim();
+        String password = passwordText.getText().toString().trim();
+
+        fireBaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = fireBaseAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == loginButton) {
+            signIn();
+        }
+        if (view == signUpButton) {
+            startActivity(new Intent(this, RegistrationActivity.class));
+        }
+    }
 }
