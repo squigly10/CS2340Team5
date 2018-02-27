d885fe7bb72ac1e4c704fdd85ac8970b67ea8cd4
yasaad <youssef.a.asaad@gmail.com>
2018-02-27 16:42:29 -0500

    populateShelters method done and Shelter method adjusted so capacity is a string

--- High5/app/src/main/java/com/high5/a2340/high5/Model/Model.java
+++ High5/app/src/main/java/com/high5/a2340/high5/Model/Model.java
@@ -1,12 +1,13 @@
 package com.high5.a2340.high5.Model;
 
+import android.util.Log;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.StringBufferInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,45 +22,56 @@ public class Model {
                                                                             UserTypes.ADMIN.getValue(),
                                                                             UserTypes.EMPLOYEE.getValue());
     public List<Shelter> shelterList;
+    public List<String> shelterKeys;
+    public List defaultValues;
     private DatabaseReference mDatabase;
 
     public void populateShelters() {
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        final List shelterSpecs = new ArrayList();
-        shelterSpecs.add("Address");
-        shelterSpecs.add("Capacity");
-        shelterSpecs.add("Latitude");
-        shelterSpecs.add("Longitude");
-        shelterSpecs.add("Phone Number");
-        shelterSpecs.add("Restrictions");
-        shelterSpecs.add("Shelter Name");
-        shelterSpecs.add("Special Notes");
+        shelterKeys = new ArrayList<>();
+        shelterList = new ArrayList<>();
+        defaultValues = new ArrayList();
+        shelterKeys.add("Address");
+        defaultValues.add("No Value");
+        shelterKeys.add("Capacity");
+        defaultValues.add("No Value");
+        shelterKeys.add("Latitude");
+        defaultValues.add(0.0);
+        shelterKeys.add("Longitude");
+        defaultValues.add(0.0);
+        shelterKeys.add("Phone Number");
+        defaultValues.add("No Value");
+        shelterKeys.add("Restrictions");
+        defaultValues.add("No Value");
+        shelterKeys.add("Shelter Name");
+        defaultValues.add("No Value");
+        shelterKeys.add("Special Notes");
+        defaultValues.add("No Value");
 
-        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+        mDatabase.child("shelter-data").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                DataSnapshot shelters = dataSnapshot.child("shelter-data");
-                for (DataSnapshot shelter : shelters.getChildren()) {
+                for (DataSnapshot shelter : dataSnapshot.getChildren()) {
+                    List shelterSpecs = new ArrayList();
                     int index = 0;
-                    for (Object value : shelterSpecs) {
-                        if (shelter.child((String) value).getValue() != null
-                                && !(shelter.child((String) value).equals(""))) {
-                            shelterSpecs.remove(index);
-                            shelterSpecs.add(index, shelter.child((String) value).getValue());
+                    for (String value : shelterKeys) {
+                        if (shelter.child(value).getValue() != null
+                                && !(shelter.child(value).equals(""))) {
+                            shelterSpecs.add(shelter.child(value).getValue());
                         } else {
-                            shelterSpecs.remove(index);
-                            shelterSpecs.add(index, "No Value");
+                            shelterSpecs.add(defaultValues.get(index));
                         }
                         index++;
                     }
-                    shelterList.add(new Shelter((String) shelterSpecs.get(0)
-                            , (Integer) shelterSpecs.get(1)
-                            , (Double) shelterSpecs.get(2)
-                            , (Double) shelterSpecs.get(3)
-                            , (String) shelterSpecs.get(4)
-                            , (String) shelterSpecs.get(5)
-                            , (String) shelterSpecs.get(6)
-                            , (String) shelterSpecs.get(7)));
+
+                    shelterList.add(new Shelter((String) shelterSpecs.get(0),
+                            String.valueOf(shelterSpecs.get(1)),
+                            (Double) shelterSpecs.get(2),
+                            (Double) shelterSpecs.get(3),
+                            (String) shelterSpecs.get(4),
+                            (String) shelterSpecs.get(5),
+                            (String) shelterSpecs.get(6),
+                            (String) shelterSpecs.get(7)));
                 }
 
             }
