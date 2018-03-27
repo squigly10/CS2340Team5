1c1937d9bc7157af5d8e9346ad547d420cf66fb4
hjsmulders <hjsmulders@gmail.com>
2018-03-27 16:23:08 -0400

    added a map acitvity and layout; no way to get to them currently

---
+++ High5/app/src/main/java/com/high5/a2340/high5/Activities/MapActivity.java
@@ -0,0 +1,50 @@
+package com.high5.a2340.high5.Activities;
+
+/**
+ * Created by hsmul on 3/27/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.high5.a2340.high5.Model.Shelter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private List<Shelter> shelterList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        shelterList = (List<Shelter>) getIntent().getSerializableExtra("shelterList");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a markers
+        for (Shelter e : shelterList) {
+            LatLng pos = new LatLng(e.getLatitude(), e.getLatitude());
+            mMap.addMarker(new MarkerOptions().position(pos).title(e.getShelterName()));
+        }
+        //move camera to focus on first shelter in the list
+        Shelter example = shelterList.get(0);
+        LatLng pos = new LatLng(example.getLatitude(), example.getLatitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng());
+    }
+}
