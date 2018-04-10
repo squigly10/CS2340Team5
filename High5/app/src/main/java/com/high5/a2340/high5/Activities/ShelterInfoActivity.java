3355166e2d542929d317117ab4cdb374fc359ab4
tom <tom.hightower356@gmail.com>
2018-04-09 22:27:39 -0400

    Tried to fix law of demeter error; not sure if it worked

--- High5/app/src/main/java/com/high5/a2340/high5/Activities/ShelterInfoActivity.java
+++ High5/app/src/main/java/com/high5/a2340/high5/Activities/ShelterInfoActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.high5.a2340.high5.Model.AgeRange;
 import com.high5.a2340.high5.Model.Shelter;
 import com.high5.a2340.high5.R;
 
@@ -52,7 +53,8 @@ public class ShelterInfoActivity extends AppCompatActivity implements View.OnCli
         boolean isFemale = currentShelter.isFemale();
         boolean isMale = currentShelter.isMale();
         String specialNotes = currentShelter.getSpecialNotes();
-        String shelterAgeRange = currentShelter.getAgeRange().toString();
+        AgeRange shelterRange = currentShelter.getAgeRange();
+        String shelterAgeRange = shelterRange.toString();
         Integer currentAvailability = currentShelter.getCurrentAvailability();
         shelterID = currentShelter.getShelterID();
 
